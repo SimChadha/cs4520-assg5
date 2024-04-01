72009abf2393d02ef6e89e97c59f88eba6bc1c96
Simar Chadha <simarchadha1@gmail.com>
2024-03-31 21:37:47 -0400

    Clean up and image ratio fix

--- app/src/main/java/com/cs4520/assignment5/ProductScreen.kt
+++ app/src/main/java/com/cs4520/assignment5/ProductScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -68,7 +69,8 @@ fun ProductRow(product: Product) {
         Image(
             painter = painterResource(id = img),
             contentDescription = stringResource(id = desc),
-            modifier = Modifier.size(100.dp))
+            modifier = Modifier.size(100.dp),
+            contentScale = ContentScale.Crop)
         Column {
             Text(text = product.name)
             Text(text = product.price.toString())
